74bf6f8a02b112d351eccbf1acf0cde6205fea84
ShinEndo <endo@elplanning.co.jp>
2023-08-31 08:44:54 +0900

    第2章　非同期プログラミング - 混ぜるな危険、同期と非同期

--- ch02/02/03/index.ts
+++ ch02/02/03/index.ts
@@ -1,5 +1,3 @@
-
-
 // 2.2.3　混ぜるな危険、同期と非同期
 // *************************************************
 function parseJSONAsync(
@@ -13,31 +11,34 @@ function parseJSONAsync(
 			callback(err);
 		}
 	}, 1000);
-};
+}
 
 const cache2 = {};
 
-function parseJSONAsyncWithCache(json: string,callback:any){
-    const cached = cache2[json];
+function parseJSONAsyncWithCache(json: string, callback: any) {
+	const cached = cache2[json];
 
-    if(cached) {
-        // キャッシュに値が存在する場合でも、非同期的にコールバックを実行する
-        setTimeout(()=>callback(cached.err,cached.result),0);
-        return;
-    }
+	if (cached) {
+		// キャッシュに値が存在する場合でも、非同期的にコールバックを実行する
+		setTimeout(() => callback(cached.err, cached.result), 0);
+		return;
+	}
 
-    parseJSONAsync(json,(err,result)=>{
-        cache2[json] = {err,result};
-        callback(err,result);
-    });
+	parseJSONAsync(json, (err, result) => {
+		cache2[json] = { err, result };
+		callback(err, result);
+	});
 }
 
-parseJSONAsyncWithCache('{"message":"Hello","to":"World"}',(err,result) => {
-    console.log('1回目の結果',err,result);
+parseJSONAsyncWithCache('{"message":"Hello","to":"World"}', (err, result) => {
+	console.log('1回目の結果', err, result);
 
-    parseJSONAsyncWithCache('{"message":"Hello","to":"World"}',(err,result) => {
-        console.log('2回目の結果',err,result);
-    });
-    console.log('2回目の呼び出し完了');
+	parseJSONAsyncWithCache(
+		'{"message":"Hello","to":"World"}',
+		(err, result) => {
+			console.log('2回目の結果', err, result);
+		}
+	);
+	console.log('2回目の呼び出し完了');
 });
 console.log('1回目の呼び出し完了');
