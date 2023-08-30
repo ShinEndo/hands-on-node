843bb3988652797d3bad31c5f3639ef9caa3e0f2
ShinEndo <endo@elplanning.co.jp>
2023-08-30 09:04:12 +0900

    2.2.1 コールバックを利用した非同期APIを実行する

--- ch02/02/01/index.js
+++ ch02/02/01/index.js
@@ -1,6 +1,24 @@
+"use strict";
 // 2.2.1　コールバックを利用した非同期APIを実行する
 // *************************************************
-setTimeout(function () {
-    console.log('5秒経過しました');
-}, 5000);
+setTimeout(() => {
+    console.log('1秒経過しました。');
+}, 1000);
 console.log('setTimeout()を実行しました。');
+const array1 = [0, 1, 2, 3];
+const array2 = array1.map((element) => {
+    console.log(`${element}を変換します。`);
+    return element * 10;
+});
+console.log('配列の変換が完了しました。', array2);
+const fs = require('fs');
+fs.readdir('.', (err, files) => {
+    console.log('fs.readdir()実行結果');
+    console.log('err', err);
+    console.log('files', files);
+});
+fs.readdir('foo', (err, files) => {
+    console.log('fs.readdir()実行結果');
+    console.log('err', err);
+    console.log('files', files);
+});
