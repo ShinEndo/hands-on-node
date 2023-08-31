2682bd08d06a0fed2a3b33a305bdb8eafb0e67d2
ShinEndo <endo@elplanning.co.jp>
2023-08-31 09:02:03 +0900

    第2章　非同期プログラミング - Promiseインスタンスの生成と状態遷移

---
+++ ch02/03/01/index.ts
@@ -0,0 +1,27 @@
+// 2.3.1　Promiseインスタンスの生成と状態遷移
+// *************************************************
+function parseJSONAsync(json: string) {
+	// Promiseインスタンスを生成して返す（この時点ではpending状態）
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			try {
+				// fullfilled状態にする（解決）
+				resolve(JSON.parse(json));
+			} catch (err) {
+				// reject状態にする
+				reject(err);
+			}
+		}, 1000);
+	});
+}
+
+const toBeFulfilled = parseJSONAsync('{"foo":1}');
+// const toBeRejected = parseJSONAsync('不正なJSON');
+console.log('******************** Promise生成直後 ********************');
+console.log(toBeFulfilled);
+// console.log(toBeRejected);
+setTimeout(() => {
+	console.log('******************** 1秒後 ********************');
+	console.log(toBeFulfilled);
+	// console.log(toBeRejected);
+}, 1000);
