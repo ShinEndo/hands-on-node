185b9258644ffec869ac426f2626d5ea05e07639
Shin Endo <end3mame@gmail.com>
2023-09-02 16:00:52 +0900

    第2章　非同期プログラミング - catch()

--- ch02/03/02/index.js
+++ ch02/03/02/index.js
@@ -9,5 +9,36 @@ console.log(numberPromise, ':numberPromise');
 console.log(stringPromise, ':stringPromise');
 setTimeout(function () { return console.log(numberPromise, ':numberPromise'); }, 0);
 setTimeout(function () { return console.log(stringPromise, ':stringPromise'); }, 0);
-var unrecoveredPromise = Promise.reject(new Error('エラー')).then(function () { return 1; });
-console.log(unrecoveredPromise);
+// const unrecoveredPromise = Promise.reject(new Error('エラー')).then(() => 1);
+// setTimeout(() => console.log(unrecoveredPromise, 'unrecoveredPromise'),0);
+var recoveredPromise = Promise.reject(new Error('エラーメッセージ')).then(function () { return 1; }, function (err) { return err.message; });
+setTimeout(function () { return console.log(recoveredPromise, 'recoveredPromise'); }, 0);
+// const rejectedPromise = stringPromise.then(()=>{throw new Error('エラー')});
+// setTimeout(() => console.log(rejectedPromise, 'rejectedPromise'),0);
+function parseJSONAsync(json) {
+    // Promiseインスタンスを生成して返す（この時点ではpending状態）
+    return new Promise(function (resolve, reject) {
+        return setTimeout(function () {
+            try {
+                // fullfilled状態にする（解決）
+                resolve(JSON.parse(json));
+            }
+            catch (err) {
+                // reject状態にする（拒否）
+                reject(err);
+            }
+        }, 1000);
+    });
+}
+var objPromise = stringPromise.then(parseJSONAsync);
+objPromise;
+var rejectedObjPromsie = Promise.resolve('不正なJSON').then(parseJSONAsync);
+rejectedObjPromsie;
+// 2.3.2.2　catch()
+// *************************************************
+// then()でonFulfilled()を省略
+var withoutOnFulfilled = Promise.reject(new Error('エラー')).then(undefined, function () { return 0; });
+withoutOnFulfilled;
+// catch()の利用で同じ処理になる
+var catchedPromise = Promise.reject(new Error('エラー')).catch(function () { return 0; });
+catchedPromise;
--- ch02/03/02/index.ts
+++ ch02/03/02/index.ts
@@ -11,5 +11,48 @@ console.log(stringPromise, ':stringPromise');
 setTimeout(() => console.log(numberPromise, ':numberPromise'), 0);
 setTimeout(() => console.log(stringPromise, ':stringPromise'), 0);
 
-const unrecoveredPromise = Promise.reject(new Error('エラー')).then(() => 1);
-console.log(unrecoveredPromise);
+// const unrecoveredPromise = Promise.reject(new Error('エラー')).then(() => 1);
+// setTimeout(() => console.log(unrecoveredPromise, 'unrecoveredPromise'),0);
+
+const recoveredPromise = Promise.reject(new Error('エラーメッセージ')).then(()=>1,err => err.message);
+setTimeout(() => console.log(recoveredPromise, 'recoveredPromise'),0);
+
+// const rejectedPromise = stringPromise.then(()=>{throw new Error('エラー')});
+// setTimeout(() => console.log(rejectedPromise, 'rejectedPromise'),0);
+
+function parseJSONAsync(json: string) {
+	// Promiseインスタンスを生成して返す（この時点ではpending状態）
+	return new Promise((resolve, reject) =>
+		setTimeout(() => {
+			try {
+				// fullfilled状態にする（解決）
+				resolve(JSON.parse(json));
+			} catch (err) {
+				// reject状態にする（拒否）
+				reject(err);
+			}
+		}, 1000)
+	);
+}
+
+const objPromise = stringPromise.then(parseJSONAsync);
+objPromise;
+
+const rejectedObjPromsie = Promise.resolve('不正なJSON').then(parseJSONAsync);
+rejectedObjPromsie;
+
+// 2.3.2.2　catch()
+// *************************************************
+// then()でonFulfilled()を省略
+const withoutOnFulfilled = Promise.reject(new Error('エラー')).then(undefined,()=>0);
+withoutOnFulfilled;
+// catch()の利用で同じ処理になる
+const catchedPromise = Promise.reject(new Error('エラー')).catch(()=>0);
+catchedPromise;
+
+
+
+
+
+
+
