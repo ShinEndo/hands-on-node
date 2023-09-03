9e9a013a59cc47d194d29d4710714d28d7a48dc4
Shin Endo <end3mame@gmail.com>
2023-09-03 10:38:50 +0900

    第2章　非同期プログラミング - ジェネレータを利用した非同期プログラミング

--- ch02/04/01/index.js
+++ ch02/04/01/index.js
@@ -69,3 +69,53 @@ const tryCatchGenerator = tryCatchGeneratorFunc();
 tryCatchGenerator.next();
 tryCatchGenerator.throw(new Error('エラー'));
 tryCatchGenerator.next();
+// 2.4.4　ジェネレータを利用した非同期プログラミング
+// *************************************************
+function parseJSONAsync(json) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                resolve(JSON.parse(json));
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+    });
+}
+function* asyncWithGeneratorFunc(json) {
+    try {
+        const result = yield parseJSONAsync(json);
+        console.log('パース結果', result);
+    }
+    catch (err) {
+        console.log('エラーをキャッチ', err);
+    }
+}
+// 正常系
+const asyncWithGenerator1 = asyncWithGeneratorFunc('{"foo":1}');
+const promise1 = asyncWithGenerator1.next().value;
+if (promise1)
+    promise1.then(result => asyncWithGenerator1.next(result));
+// 異常系
+const asyncWithGenerator2 = asyncWithGeneratorFunc('不正なJSON');
+const promise2 = asyncWithGenerator2.next().value;
+if (promise2)
+    promise2.catch(err => asyncWithGenerator2.throw(err));
+// 非同期処理を実行するジェネレータの汎用的なハンドリング関数
+// 戻り値はPromsieインスタンス
+function handleAsyncWithGenerator(generator, resolved = null) {
+    // 前回yieldされたPromiseインスタンスの値を引数にnext()を実行
+    // 初回はresolvedには値が入っていない（undefined)
+    const { done, value } = generator.next(resolved);
+    if (done) {
+        return Promise.resolve(value);
+    }
+    return value.then(
+    // 正常系では再起呼び出し
+    resolved => handleAsyncWithGenerator(generator, resolved), 
+    // 異常系ではthrow()メソッドを実行
+    err => generator.throw(err));
+}
+handleAsyncWithGenerator(asyncWithGeneratorFunc('{"foo":1}'));
+handleAsyncWithGenerator(asyncWithGeneratorFunc('不正なJSON'));
