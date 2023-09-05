9b4621175d0461abd70551765a9b4443616039b5
Shin Endo <end3mame@gmail.com>
2023-09-05 22:25:22 +0900

    第3章　EventEmitterとストリーム - EventEmitterの継承

--- ch03/01/index.ts
+++ ch03/01/index.ts
@@ -84,11 +84,7 @@ function endListener(this: any) {
 	console.log('end');
 	// thisはEventEmitterインスタンス
 	// すべてのイベントからリスナを削除する
-	this.off('start', startListener)
-		.off('Fizz', fizzListener)
-		.off('Buzz', BuzzListener)
-		.off('FFizzBuzz', fizzBuzzListener)
-		.off('end', endListener);
+	this.off('start', startListener).off('Fizz', fizzListener).off('Buzz', BuzzListener).off('FFizzBuzz', fizzBuzzListener).off('end', endListener);
 }
 
 createFizzBuzzEventEmitter(40)
@@ -146,3 +142,30 @@ bazEventListener.emit('baz');
 // 3.1.3　エラーハンドリング
 // *************************************************
 // → error-events.ts参照
+
+// 3.1.4　EventEmitterの継承
+// *************************************************
+class FizzBuzzEventEmitter extends events.EventEmitter {
+	async start(until) {
+		this.emit('start');
+		let count = 1;
+		while(true) {
+			if(count % 15 === 0) {
+				this.emit('FizzBuzz',count);
+			} else if (count % 3 === 0) {
+				this.emit('Fizz',count);
+			} else if (count % 5 === 0) {
+				this.emit('Buzz',count);
+			}
+			count += 1;
+
+			if(count > until) {
+				break;
+			}
+			await new Promise(resolve => setTimeout(resolve,100));
+		}
+		this.emit('end');
+	}
+}
+
+new FizzBuzzEventEmitter().on('start',startListener).on('Fizz',fizzListener).on('Buzz',BuzzListener).on('FizzBuzz',fizzBuzzListener).on('end',endListener).start(100);
