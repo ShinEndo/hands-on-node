67a5691a55047ebc9daedfbaa3c06f121b86217c
Shin Endo <end3mame@gmail.com>
2023-09-06 23:17:38 +0900

    第3章　EventEmitterとストリーム - 読み込みストリーム

--- ch03/02/index.js
+++ ch03/02/index.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 // 3　EventEmitterとストリーム
 // *************************************************
@@ -25,3 +40,49 @@ fs.writeFileSync('src.txt', 'Hello,World!');
 copyFileWithStream('src.txt', 'dest.txt', function () { return console.log('コピー完了'); });
 var crypto = require("crypto");
 fs.createReadStream('src.txt').pipe(crypto.createHash('sha256')).pipe(fs.createWriteStream('dest.txt')).on('finish', function () { return console.log('コピー完了'); });
+// 3.2.2　読み込みストリーム
+// *************************************************
+var readStream = fs.createReadStream('src.txt');
+readStream.on('readable', function () {
+    console.log('readble');
+    var chunk;
+    // 現在読み込み可能なデータをすべて読み込む
+    while ((chunk = readStream.read()) !== null) {
+        console.log("chunk: ".concat(chunk.toString()));
+    }
+}).on('end', function () { return console.log('end'); });
+// node --experimental-repl-await
+var stream = require("stream");
+var HelloReadbleStream = /** @class */ (function (_super) {
+    __extends(HelloReadbleStream, _super);
+    function HelloReadbleStream(options) {
+        var _this = _super.call(this, options) || this;
+        _this.languages = ['JavaScript', 'Python', 'Java', 'C#'];
+        return _this;
+    }
+    HelloReadbleStream.prototype._read = function (size) {
+        console.log('_read()');
+        var language;
+        while ((language = this.languages.shift())) {
+            // push()でデータを流す
+            // ただし、push()がfalseを返したらそれ以上流さない
+            if (!this.push("Hello, ".concat(language, "!\n"))) {
+                console.log('読み込み中断');
+                return;
+            }
+        }
+        // 最後にnullを流してストリームの終了を通知する
+        console.log('読み込み完了');
+        this.push(null);
+    };
+    return HelloReadbleStream;
+}(stream.Readable));
+var options = {};
+var helloReadableStream = new HelloReadbleStream(options);
+helloReadableStream.on('readable', function () {
+    console.log('readable');
+    var chunk;
+    while ((chunk = helloReadableStream.read()) !== null) {
+        console.log("chunk: ".concat(chunk.toString()));
+    }
+}).on('end', function () { return console.log('end'); });
--- ch03/02/index.ts
+++ ch03/02/index.ts
@@ -28,3 +28,50 @@ copyFileWithStream('src.txt','dest.txt',()=>console.log('コピー完了'));
 import * as crypto from 'crypto';
 fs.createReadStream('src.txt').pipe(crypto.createHash('sha256')).pipe(fs.createWriteStream('dest.txt')).on('finish',()=>console.log('コピー完了'));
 
+// 3.2.2　読み込みストリーム
+// *************************************************
+const readStream = fs.createReadStream('src.txt');
+readStream.on('readable',()=>{
+    console.log('readble');
+    let chunk;
+    // 現在読み込み可能なデータをすべて読み込む
+    while((chunk = readStream.read()) !== null) {
+        console.log(`chunk: ${chunk.toString()}`);
+    }
+}).on('end',()=>console.log('end'));
+
+// node --experimental-repl-await
+import * as stream from 'stream';
+class HelloReadbleStream extends stream.Readable {
+
+    languages: string[];
+    constructor(options) {
+        super(options);
+        this.languages = ['JavaScript','Python','Java','C#'];
+    }
+
+    _read(size) {
+        console.log('_read()');
+        let language;
+        while((language = this.languages.shift())){
+            // push()でデータを流す
+            // ただし、push()がfalseを返したらそれ以上流さない
+            if(!this.push(`Hello, ${language}!\n`)){
+                console.log('読み込み中断');
+                return;
+            }
+        }
+        // 最後にnullを流してストリームの終了を通知する
+        console.log('読み込み完了');
+        this.push(null);
+    }
+}
+const options = {};
+const helloReadableStream = new HelloReadbleStream(options);
+helloReadableStream.on('readable',()=>{
+    console.log('readable');
+    let chunk;
+    while((chunk = helloReadableStream.read()) !== null) {
+        console.log(`chunk: ${chunk.toString()}`);
+    }
+}).on('end',()=>console.log('end'))
