4434c3f3a34c130c7cd760059e8a7fa063e6f3f8
ShinEndo <endo@elplanning.co.jp>
2023-10-02 09:08:02 +0900

    第4章　マルチプロセス、マルチスレッド  - スレッド間での値の共有

--- ch04/multi-thread-share/fibonacci.ts
+++ ch04/multi-thread-share/fibonacci.ts
@@ -8,5 +8,8 @@ const { workerData: int32Array, parentPort } = require('worker_threads');
 parentPort.on('message', (n) => {
 	parentPort.postMessage(fibonacci(n));
 	// 処理のたびに最初の値をインクリメントする
-	int32Array[0] += 1;
+	// int32Array[0] += 1;
+
+	// Atomics.add()でint32Arrayの0番目の値に1を足す
+	Atomics.add(int32Array, 0, 1);
 });
