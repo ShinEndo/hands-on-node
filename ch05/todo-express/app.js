35c8248ad326a8fe77922411f3ee4a7269b643a6
ShinEndo <endo@elplanning.co.jp>
2023-10-05 09:17:44 +0900

    第5章　HTTPサーバとHTTPクライアント - POSTリクエストのハンドリング

--- ch05/todo-express/app.js
+++ ch05/todo-express/app.js
@@ -11,6 +11,8 @@ let todos = [
 
 const app = express();
 
+app.use(express.json());
+
 // ToDo一覧の取得
 app.get('/api/todos', (req, res) => {
 	if (!req.query.completed) {
@@ -22,4 +24,31 @@ app.get('/api/todos', (req, res) => {
 	res.json(todos.filter((todo) => todo.completed === completed));
 });
 
+// ToDoのIDの値を管理するための変数
+let id = 2;
+
+// ToDoの新規登録
+app.post('/api/todos', (req, res, next) => {
+	const { title } = req.body;
+	if (typeof title !== 'string' || !title) {
+		// titleリクエストに含まれない場合はステータスコード400(Bad Request)
+		const err = new Error('title is required');
+		err.statusCode(err);
+		return next(err);
+	}
+	// ToDoの作成
+	const todo = { id: (id += 1), title, completed: false };
+	todos.push(todo);
+	// ステータスコード201(Created)で結果を返す
+	res.status(201).json(todo);
+});
+
+// エラーハンドリングミドルウェア
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(err.statusCode || 500).json({ error: err.message });
+});
+
 app.listen(3000);
+
+await fetch('http://localhost:3000/api/todos', { method: 'POST' });
