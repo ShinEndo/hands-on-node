20b9dab3c50a1a72892b04c1cdf095dd62d1f56e
ShinEndo <endo@elplanning.co.jp>
2023-10-20 14:55:03 +0900

    第9章　デプロイ - Docker

--- ch09/hello-world/app.js
+++ ch09/hello-world/app.js
@@ -1,6 +1,4 @@
 'use strict';
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello world')).listen(
-	process.env.PORT || 3000
-);
+require('express')()
+	.get('/', (req, res) => res.send('Hello world'))
+	.listen(process.env.PORT || 3000);
